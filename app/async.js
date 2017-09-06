51f8a4ec42e95dd9940b9e2fec7c989010739b0b
chrismorse <github@beeksma.com>
2017-09-06 15:41:55 -0700

    async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,25 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => {
+      resolve(value);
+    });
   },
 
   manipulateRemoteData: function(url) {
 
+    return {
+      then: function(callback) {
+        fetch(url)
+        .then( resp => resp.json())
+        .then( data => {
+          names = [];
+          data.people.forEach(item => names.push(item.name));
+          names.sort();
+          
+          return callback(names)
+        })
+      }
+    }
   }
 };
