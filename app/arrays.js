5ba867f718b8f8043f190cb5a5a417f7e0dba423
chrismorse <github@beeksma.com>
2017-09-06 13:37:09 -0700

    finished arrays section

--- app/arrays.js
+++ app/arrays.js
@@ -17,7 +17,6 @@ exports.arraysAnswers = {
     while (arr.indexOf(item) > 0) {
       arr.splice(arr.indexOf(item),1);
     }
-
     return arr;
   },
 
@@ -27,38 +26,64 @@ exports.arraysAnswers = {
   },
 
   truncate: function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count: function(arr, item) {
-
+    sum = 0;
+    arr.forEach(arrItem => {
+      if (arrItem === item)
+        sum++
+    });
+    return sum;
   },
 
   duplicates: function(arr) {
 
+    var notDups = new Set();
+    var dups = new Set();
+
+    for (let i = 0; i < arr.length; i++) {
+      thisVal = arr[i];
+      if (notDups.has(thisVal))
+        dups.add(thisVal)
+      else
+        notDups.add(thisVal);
+    }
+    return [...dups];
   },
 
   square: function(arr) {
-
+    return arr.map(item => item * item)
   },
 
   findAllOccurrences: function(arr, target) {
+    index = [];
+    arr.forEach((item, i) => {
+      if (target === item)
+        index.push(i);
+    });
 
+    return index;
   }
 };
