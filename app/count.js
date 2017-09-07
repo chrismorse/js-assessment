becbb60563bcbd94cc55f276e05065f22835c3c4
chrismorse <github@beeksma.com>
2017-09-07 08:40:55 -0700

    timer; fixing tests

--- app/count.js
+++ app/count.js
@@ -3,5 +3,23 @@ exports = typeof window === 'undefined' ? global : window;
 exports.countAnswers = {
   count: function (start, end) {
 
+    let timeout;
+
+    const logIt = () => {
+      console.log(start++);
+
+      if (start <= end) {
+        timeout = setTimeout(logIt, 100);
+      }
+    }
+
+    logIt();
+
+    return {
+      cancel: function () {
+        clearTimeout(timeout)
+      }
+    }
+
   }
 };
